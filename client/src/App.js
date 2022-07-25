1b056bdd453391617007eede342c6fa3a2274ff5
Lauren Grubb <lauren.elyse.grubb@gmail.com>
2022-07-25 14:59:22 -0400

    backend and front end setup

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom'
+
+const App = () => {
+    
+  return (
+      <div className='page'>
+        
+        <main> 
+
+            <Routes>
+            </Routes>
+
+        </main>
+      </div>
+    )
+  }
+  export default App
---
+++ seed/villagers.js
@@ -0,0 +1,57 @@
+const { mainModule } = require('process')
+const db = require('../db')
+const { Rating, Review } = require('../models')
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+const villager = async () => {
+
+    Villager.Collection.drop()
+
+    const villagers = [
+        {
+            _id: "a",
+            name: "a",
+            type:"a",
+            reviews: [a],
+            rating: [a],
+        },
+        {
+            _id: "b",
+            name: "b",
+            type:"b",
+            reviews: [b],
+            rating: [b],
+        },
+        {
+            _id: "c",
+            name: "c",
+            type:"c",
+            reviews: [c],
+            rating: [c],
+        },
+        {
+            _id: "d",
+            name: "d",
+            type:"d",
+            reviews: [d],
+            rating: [d],
+        },
+        {
+            _id: "e",
+            name: "e",
+            type:"e",
+            reviews: [e],
+            rating: [e],
+        },
+    ]
+    await Villager.insertMany(villagers)
+    console.log('Populated Villagers!')
+}
+
+const run = async () => {
+    await main()
+    db.close()
+    await villager()
+}
+run()
