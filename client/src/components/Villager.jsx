480883c3772e8f8d825e0794d025a5e89b9c30f7
Lauren Grubb <lauren.elyse.grubb@gmail.com>
2022-07-25 23:02:54 -0400

    made a villager component

---
+++ client/src/components/Villager.jsx
@@ -0,0 +1,21 @@
+
+const villagerCard = ({ info }) => {
+
+ 
+   
+    return (
+      <div className="villagerCard"  >
+        
+        <p className='name'>{ villager.name }</p>
+        <img src={villager.image} alt="image"  ></img>
+    
+        <div className="info-wrapper flex-col">
+      
+        </div>
+      </div>
+  
+      )
+    }
+    export default villagerCard
+  
+  
