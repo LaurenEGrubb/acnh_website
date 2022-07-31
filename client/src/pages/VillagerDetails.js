58efe896e1301d35cd25309a2463d75da0a12a42
Lauren Grubb <lauren.elyse.grubb@gmail.com>
2022-07-31 14:31:39 -0400

    got review to render on the page

--- client/src/pages/VillagerDetails.js
+++ client/src/pages/VillagerDetails.js
@@ -1,57 +1,67 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import Review from '../components/Review'
+//import Review from '../components/Review'
 import { useParams } from 'react-router-dom'
+//import review from '../models/review'
+//import { schema } from '../../../models/villager.js'
 
 
 
 
-const VillagerDetails = ({ villagerDetails, setVillagerDetails }) => {
+
+const VillagerDetails = () => {
     const { id } = useParams()
     const [villager, setVillager] = useState({})
+    const [message, setMessage] = useState("") 
     const getVillagerDetails = async () => {
         const res = await axios.get(`http://localhost:3003/villagers/${id}`)
+        console.log(res) 
         setVillager(res.data)
+       
     }
 
     useEffect( ( ) => {
         getVillagerDetails()
     }, [])
 
- const handleDelete = async () => {
+ const handleDelete = async (id) => {
     await axios.delete(`http://localhost:3003/villagers/${id}/review`)
 }
 
-const [form, setForm] = useState(initialState);
-const handleChange = (event) => {
-    await axios.put(`http://localhost:3003/villagers/${id}/review`)
-    setForm(...form, [event.target.value]);
-}
-const handleSubmit = async (e) => {
-    e.preventDefault();
-    let response = await axios.post(`http://localhost:3003/villagers/${id}/review`, form)
+const handleSubmit = async (event) => {
+    event.preventDefault();
+   
+    let res = await axios.post(`http://localhost:3003/villagers/${id}/review`, {content:message})
+    setMessage("");
 }
 
+//handleEdit
     return (
         <div className='details'>
            
          <h2>{villager.name}</h2>
          <img src={villager.image} class="biggerimage"/>
          <p>{villager.description}</p>
-         <form onSubmit>
-            <label htmlFor="review" ></label>
-            <textarea 
+         <form onSubmit={handleSubmit}>
+            <label htmlFor="reviews" ></label>
+            <textarea onChange={(e) => setMessage(e.target.value)} value={ message }
             id="message"
             cols="30" 
             rows="10"
-            value= { Review }
+            
         >
             </textarea> 
-            <button type="submit" onClick={handleSubmit}>Submit</button>
-            <button type="submit" onClick={handleDelete}>Delete</button>
-            <button type="submit" onChange={handleChange}>Edit</button>
+            <button type="submit">Submit</button>
+           
          </form>
-                        
+            
+           <button type="submit">Edit</button>   
+           {villager.reviews?.map((review) => (
+            <div>
+                <p>{review.content}</p>
+                <button type="submit" onClick={()=>handleDelete(review._id)}>Delete</button>
+            </div>
+           ))}          
         </div>
     )
 }
