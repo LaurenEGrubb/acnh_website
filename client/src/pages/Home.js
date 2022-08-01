4e1c95fe5ba535ef74d6e7e23f4cddd7e646a82f
Lauren Grubb <lauren.elyse.grubb@gmail.com>
2022-08-01 08:20:30 -0400

    some instructions on the home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ const Home = (  ) => {
     return (
         <div className='home'>
             <h1> Animal Crossing</h1>
-                <p> rate ur villagers
+                <p> Please click the url that says Viillagers. There you'll be able to select one and be taken to their review page.
                 </p>
             
         </div>
